4113d6692c27a7c68674dbad3defa05887123008
Ved Pawar <vedpawar1410@gmail.com>
2025-09-22 22:04:49 +0530

    while loop added Game can runs 5 times

--- javascript.js
+++ javascript.js
@@ -1,68 +1,70 @@
 // display an intro message
 console.log("Welcome to Rock Paper Scissors");
 
-//get computer choice
-let computerTextChoice = function getComputerChoice() {
-    
-    let computerNumberChoice = Math.floor(Math.random() * 3) + 1;
-    if(computerNumberChoice === 1){
-        return "ROCK";
-    }
-    else if(computerNumberChoice === 2){
-        return "SCISSORS";
-    }
-    else if(computerNumberChoice === 3){
-        return "PAPER";
-    }
-    
-};
+let x = 5; //amount of rounds you want to play
 
-/* console.log("Computer Chooses: ")
-console.log(computerTextChoice()); //To look at computers choice */
+while (x > 0) {
+    // get computer choice
+    let computerTextChoice = function getComputerChoice() {
+        let computerNumberChoice = Math.floor(Math.random() * 3) + 1;
+        if (computerNumberChoice === 1) {
+            return "ROCK";
+        }
+        else if (computerNumberChoice === 2) {
+            return "SCISSORS";
+        }
+        else if (computerNumberChoice === 3) {
+            return "PAPER";
+        }
+    };
 
-//get human choice
-function getHumanChoice(){
+    // get human choice
+    function getHumanChoice() {
+        let humanChoice = prompt("Choose (Rock-paper-scissors)", "");
+        console.log("User Choose: ");
+        console.log(humanChoice);
+        return humanChoice;
+    }
 
-    let humanChoice = prompt("Choose (Rock-papers-scissors)", "");
-    console.log("User Choose: ")
-    console.log(humanChoice);
-    return humanChoice;
+    let humanFinalChoice = getHumanChoice();
+    let computerFinalChoice = computerTextChoice();
+
+    // scoreboard
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // rounds
+    console.log("Computer Choose: ");
+    console.log(computerFinalChoice);
+
+    function playRound(computerFinalChoice, humanFinalChoice) {
+        const comp = computerFinalChoice.toUpperCase();
+        const human = humanFinalChoice.toUpperCase();
+
+        if (comp === human) {
+            console.log("Draw");
+        }
+        else if (comp === 'ROCK' && human === 'PAPER') {
+            humanScore++;
+            console.log("HUMAN wins");
+        }
+        else if (comp === 'PAPER' && human === 'SCISSORS') {
+            humanScore++;
+            console.log("HUMAN wins");
+        }
+        else if (comp === 'SCISSORS' && human === 'ROCK') {
+            humanScore++;
+            console.log("HUMAN wins");
+        }
+        else {
+            computerScore++;
+            console.log("Computer wins");
+        }
+
+        console.log(`Human Score: ${humanScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+    }
+
+    playRound(computerFinalChoice, humanFinalChoice);
+    x--;
 }
-const humanFinalChoice = getHumanChoice();
-const computerFinalChoice = computerTextChoice();
-
-//scoreboard
-let humanScore=0;
-let computerScore=0;
-
-//rounds
-console.log("Computer Choose: ")
-console.log(computerFinalChoice);
-
-function playRound(computerFinalChoice, humanFinalChoice){
-    const comp = computerFinalChoice.toUpperCase();
-    const human = humanFinalChoice.toUpperCase();
-
-    if(comp === human){
-        console.log("Draw");
-    }
-    else if(comp === 'ROCK' && human==='PAPER'){
-        humanScore++;
-        console.log("HUMAN wins");
-    }
-    else if(comp === 'PAPER' && human==='SCISSORS'){
-        humanScore++;
-        console.log("HUMAN wins");
-    }
-    else if(comp === 'SCISSORS' && human==='ROCK'){
-        humanScore++;
-        console.log("HUMAN wins");
-    }
-    else{
-        computerScore++;
-        console.log("Computer wins");
-    }
-    console.log(`Human Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
-}
-playRound(computerFinalChoice, humanFinalChoice);
