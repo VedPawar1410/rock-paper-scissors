35ee61183f657a8a36df770176ee4cf15bf3ef37
Ved Pawar <vedpawar1410@gmail.com>
2025-09-22 20:20:35 +0530

    computer chooses a value(Rock-paper-scissor) function to store the result of computers choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
 // display an intro message
 console.log("Welcome to Rock Paper Scissors");
 
+//get computer choice
+let computerTextChoice = function getComputerChoice() {
+    
+    let computerNumberChoice = Math.floor(Math.random() * 3) + 1;
+    if(computerNumberChoice === 1){
+        return "Rock";
+    }
+    else if(computerNumberChoice === 2){
+        return "Scissors";
+    }
+    else if(computerNumberChoice === 3){
+        return "Paper";
+    }
+    
+};
+computerTextChoice();
+console.log(computerTextChoice()); //To look at computers choice
