0f744dea30a5279fdce4193629706a2a5bbf5bef
Ved Pawar <vedpawar1410@gmail.com>
2025-09-28 16:23:54 +0530

    1. Indentation 2. Removed loop for 5 times

--- javascript.js
+++ javascript.js
@@ -1,72 +1,74 @@
 // display an intro message
 console.log("Welcome to Rock Paper Scissors");
 
-let x = 5; //amount of rounds you want to play
+//let x = 5; //amount of rounds you want to play
 // scoreboard
 let humanScore = 0;
 let computerScore = 0;
 
-while (x > 0) {
-    console.log("******************************************************");
-    // get computer choice
-    let computerTextChoice = function getComputerChoice() {
-        let computerNumberChoice = Math.floor(Math.random() * 3) + 1;
-        if (computerNumberChoice === 1) {
-            return "ROCK";
-        }
-        else if (computerNumberChoice === 2) {
-            return "SCISSORS";
-        }
-        else if (computerNumberChoice === 3) {
-            return "PAPER";
-        }
-    };
+/* while (x > 0) {
+    
+    //x--;
+} */
 
-    // get human choice
-    function getHumanChoice() {
-        let humanChoice = prompt("Choose (Rock-paper-scissors)", "");
-        console.log(`User Choose: ${humanChoice}`);
-        return humanChoice;
+console.log("******************************************************");
+// get computer choice
+let computerTextChoice = function getComputerChoice() {
+    let computerNumberChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerNumberChoice === 1) {
+        return "ROCK";
     }
+    else if (computerNumberChoice === 2) {
+        return "SCISSORS";
+    }
+    else if (computerNumberChoice === 3) {
+        return "PAPER";
+    }
+};
 
-    let humanFinalChoice = getHumanChoice();
-    let computerFinalChoice = computerTextChoice();
+// get human choice
+function getHumanChoice() {
+    let humanChoice = prompt("Choose (Rock-paper-scissors)", "");
+    console.log(`User Choose: ${humanChoice}`);
+    return humanChoice;
+}
+
+let humanFinalChoice = getHumanChoice();
+let computerFinalChoice = computerTextChoice();
 
     
 
-    // rounds
-    console.log(`Computer Choose: ${computerFinalChoice}`);
+// rounds
+console.log(`Computer Choose: ${computerFinalChoice}`);
 
-    function playRound(computerFinalChoice, humanFinalChoice) {
-        const comp = computerFinalChoice.toUpperCase();
-        const human = humanFinalChoice.toUpperCase();
+function playRound(computerFinalChoice, humanFinalChoice) {
+    const comp = computerFinalChoice.toUpperCase();
+    const human = humanFinalChoice.toUpperCase();
 
-        if (comp === human) {
-            console.log("Draw");
-        }
-        else if (comp === 'ROCK' && human === 'PAPER') {
-            humanScore++;
-            console.log("HUMAN wins");
-        }
-        else if (comp === 'PAPER' && human === 'SCISSORS') {
-            humanScore++;
-            console.log("HUMAN wins");
-        }
-        else if (comp === 'SCISSORS' && human === 'ROCK') {
-            humanScore++;
-            console.log("HUMAN wins");
-        }
-        else {
-            computerScore++;
-            console.log("Computer wins");
-        }
-
-        console.log(`Human Score: ${humanScore} |||| Computer Score: ${computerScore}`);
+    if (comp === human) {
+        console.log("Draw");
+    }
+    else if (comp === 'ROCK' && human === 'PAPER') {
+        humanScore++;
+        console.log("HUMAN wins");
+    }
+    else if (comp === 'PAPER' && human === 'SCISSORS') {
+        humanScore++;
+        console.log("HUMAN wins");
+    }
+    else if (comp === 'SCISSORS' && human === 'ROCK') {
+        humanScore++;
+        console.log("HUMAN wins");
+    }
+    else {
+        computerScore++;
+        console.log("Computer wins");
     }
 
-    playRound(computerFinalChoice, humanFinalChoice);
-    x--;
+    console.log(`Human Score: ${humanScore} |||| Computer Score: ${computerScore}`);
 }
+
+playRound(computerFinalChoice, humanFinalChoice);
 
 console.log("******************************************************");
 if(humanScore>computerScore){
